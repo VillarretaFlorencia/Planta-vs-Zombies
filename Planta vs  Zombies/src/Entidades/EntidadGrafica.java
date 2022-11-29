0362b201b8631ebc0e27c8e3c130745cf096cfce
ItsMassi <90329965+ItsMassi@users.noreply.github.com>
2022-11-29 09:49:51 -0300

    Documentacion 2

--- Planta vs  Zombies/src/Entidades/EntidadGrafica.java
+++ Planta vs  Zombies/src/Entidades/EntidadGrafica.java
@@ -25,29 +25,53 @@ public class EntidadGrafica {
     this.ancho = ancho;
     label = new JLabel();
   }
-
+  
+  /**
+   * Cambia la imgaen dentro del label
+   * @param imagen
+   */
   public void setImagen(String imagen) {
     label.setIcon(new ImageIcon(this.getClass().getResource(imagen)));
   }
-  
+  /**
+   * cambia la posicion en la pantalla (Pixel x Pixel)
+   * @param x
+   * @param y
+   */
   public void setPosicion(int x, int y) {
 	  posicion = new Posicion (x,y);
 	  label.setBounds(x,y, ancho, alto);
 	  label.setLocation(x, y);
   }
   
+  /**
+   * Obtiene el ancho de la entidad grafica
+   * @return ancho
+   */
   public int getAncho() {
 	    return ancho;
   }
 
+  /**
+   * Obtiene el alto de la entidad grfacica
+   * @return alto
+   */
   public int getAlto() {
     return alto;
   }
 
+  /**
+   * Obtiene el label asociado a la entidad grafica
+   * @return JLabel
+   */
   public JLabel getLabel() {
     return label;
   }
 
+  /**
+   * Obtiene la posicion en pantalla del label
+   * @return Posicion
+   */
   public Posicion getPosicion () {return posicion;}
 
   
