2bdef8872e325780196f86ba54edeed5973b5ce9
ItsMassi <90329965+ItsMassi@users.noreply.github.com>
2022-11-29 10:22:16 -0300

    Documentacion 4

--- Planta vs  Zombies/src/Fabrica/FactoryNoche.java
+++ Planta vs  Zombies/src/Fabrica/FactoryNoche.java
@@ -4,7 +4,11 @@ import Plantas.HumoSeta;
 import Plantas.Planta;
 import Plantas.SetaDesporadora;
 
-
+/**
+ * Esta clase define el comportamiento de la fabrica durante los niveles de noche
+ * 
+ *
+ */
 public class FactoryNoche extends Factory {
 
   public Planta createPlantaA() {
