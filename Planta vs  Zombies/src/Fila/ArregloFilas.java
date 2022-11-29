687efc1a7fb6d0ce092597fa098b7f1c512f9e84
ItsMassi <90329965+ItsMassi@users.noreply.github.com>
2022-11-29 10:36:28 -0300

    Documentacion 5

--- Planta vs  Zombies/src/Fila/ArregloFilas.java
+++ Planta vs  Zombies/src/Fila/ArregloFilas.java
@@ -5,7 +5,11 @@ import Plantas.Planta;
 import Proyectil.Proyectil;
 import Zombies.Zombie;
 import java.util.LinkedList;
-
+/**
+ * Esta clase define el comportamiento de las filas en conjunto dentro del nivel
+ * 
+ *
+ */
 public class ArregloFilas {
 
   protected Fila[] arregloFilas;
@@ -17,26 +21,45 @@ public class ArregloFilas {
       arregloFilas[i] = new Fila();
     }
   }
-
+/**
+ * Agrega una planta en la posicion brindada por la misma
+ * @param planta
+ */
   public void setPlanta(Planta planta) {
     int fila = planta.getPosicion().getY();
     arregloFilas[conversor.convertirFila(fila)].setPlanta(planta);
   }
-  
+  /**
+   * Agrega un zombie en la posicion brindada por el mismo
+   * @param Zombie
+   */
   public void setZombie(Zombie zombie) {
     int fila = zombie.getPosicion().getY();
     arregloFilas[conversor.convertirFila(fila)].setZombie(zombie);
   }
-
+  
+  /**
+   * Agrega un proyectil en la posicion brindada por el mismo
+   * @param Proyectil
+   */
   public void setProyectil(Proyectil proyectil) {
     int fila = proyectil.getPosicion().getY();
     arregloFilas[conversor.convertirFila(fila)].setProyectil(proyectil);
   }
   
+  /**
+   * Obtiene la fila de la posicion deseada
+   * @param Posicion de la fila
+   * @return Fila
+   */
   public Fila getFila(int i) {
     return arregloFilas[i];
   }
   
+  /**
+   * Retorna una lista con todos los zombies pertenecientes a todas todas las filas
+   * @return
+   */
   public LinkedList<Zombie> getTodosLosZombies() {
     LinkedList<Zombie> zombies = new LinkedList<Zombie>();
     for (int i = 0; i < arregloFilas.length; i++) {
--- Planta vs  Zombies/src/Fila/Fila.java
+++ Planta vs  Zombies/src/Fila/Fila.java
@@ -5,7 +5,11 @@ import Proyectil.Proyectil;
 import Zombies.Zombie;
 import java.util.Iterator;
 import java.util.LinkedList;
-
+/**
+ * Describe el comportamiento de la Fila dentro del nivel
+ * 
+ *
+ */
 public class Fila {
 
   protected LinkedList<Zombie> listaZombies;
@@ -18,54 +22,96 @@ public class Fila {
     listaPlantas = new LinkedList<Planta>();
     listaProyectiles = new LinkedList<Proyectil>();
   }
-
+  /**
+   * Agrega una planta en la posicion brindada por la misma
+   * @param planta
+   */
   public void setPlanta(Planta planta) {
     listaPlantas.add(planta);
   }
   
+  /**
+   * Agrega un zombie en la posicion brindada por el mismo
+   * @param Zombie
+   */
   public void setZombie(Zombie zombie) {
     listaZombies.add(zombie);
   }
-
+  
+  /**
+   * Agrega un proyectil en la posicion brindada por el mismo
+   * @param Proyectil
+   */
   public void setProyectil(Proyectil planta) {
     listaProyectiles.add(planta);
   }
 
+  /**
+   * Obtiene todas las plantas pertenecientes a la fila
+   * @return Lista de plantas
+   */
   public LinkedList<Planta> getPlantas() {
     return listaPlantas;
   }
   
+  /**
+   * Obtiene todas los zombies pertenecientes a la fila
+   * @return Lista de plantas
+   */
   public LinkedList<Zombie> getZombies() {
     return listaZombies;
   }
 
+  /**
+   * Obtiene todas los proyectiles pertenecientes a la fila
+   * @return Lista de plantas
+   */
   public LinkedList<Proyectil> getProyectiles() {
     return listaProyectiles;
   }
 
+  /**
+   * Elimina a la planta de dentro de la fila
+   * @param planta a eliminar
+   */
   public void sacarPlanta(Planta planta) {	 
 	  int indice = listaPlantas.indexOf(planta);
 	  if (indice >= 0)
 			listaPlantas.remove(listaPlantas.indexOf(planta));
   }
 
+  /**
+   * Elimina al zombie de dentro de la fila
+   * @param zombie a eliminar
+   */
   public void sacarZombie(Zombie zombie) {
 	  int indice = listaZombies.indexOf(zombie);
 	  if (indice >= 0)
 			listaZombies.remove(indice);
   }
 
+  /**
+   * Elimina al proyectil de dentro de la fila
+   * @param proyectil a eliminar
+   */
   public void sacarProyectil(Proyectil proyectil) {
 	int indice = listaProyectiles.indexOf(proyectil);
 	if (indice >= 0)
 		listaProyectiles.remove(indice);
   }
-
+  
+  /**
+   * Retorna un valor booleano dependiendo si hay zombies en la fila
+   * @return boolean -> zombies en fila
+   */
   public boolean hayZombies() {
 	  LinkedList<Zombie> copiaZombies = (LinkedList<Zombie>) listaZombies.clone();
 	  return !copiaZombies.isEmpty();
   }
   
+  /**
+   * Verifica las coliciones de los zombies entre proyectiles y plantas
+   */
   public void colisiones() {
 	LinkedList<Planta> copiaPlantas = (LinkedList<Planta>) listaPlantas.clone();
     LinkedList<Zombie> copiaZombies = (LinkedList<Zombie>) listaZombies.clone();
@@ -93,6 +139,9 @@ public class Fila {
     }
   } 
   
+  /**
+   * Elimina todos los componentes de la fila
+   */
   public void limpiarFila() {
     listaZombies.clear();
     listaPlantas.clear();
